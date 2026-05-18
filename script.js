2dee797805c62c39a477dd01bbd2c9092e57f866
romain42DEV <romain.guironnet42000@gmail.com>
2026-05-18 15:05:28 +0200

    store

--- script.js
+++ script.js
@@ -11,6 +11,7 @@
 	.then(data=>{
 		// a ce niveau on devrait avoir dans la console les données renvoyé par l'api fake store api
 		console.log(data)
+        afficheProduit(data)
 	});
 
 
@@ -21,7 +22,28 @@
     function afficheProduit(tableauProduits) {
         tableauProduits.forEach(produit => {
             let produitCard = `
-            
+            <div class="card w-48 flex space-between mt-10">
+                <div class="w-30">
+                    <img src="${produit.image}" alt="products" class="w-100">
+                </div>
+
+                <div class="flex space-between align-center w70">
+                    <div class="w70">
+                        <h3>${produit.title}</h3>
+                        <p class="mt-10">${produit.category}</p>
+                        <p class="mt-10"><i class="ph-fill ph-star"></i>
+                            <i class="ph-fill ph-star"></i>
+                            <i class="ph-fill ph-star"></i>
+                            <i class="ph-fill ph-star"></i>
+                        </p>
+
+                        <div class="w-30">
+                            <p class="bg-black white padding-10 border-radius-10">${produit.price}€</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
             `
-        })
+        document.querySelector("#produit-container").innerHTML += produitCard;
+        });
     }
